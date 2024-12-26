a293eb849f334fc4f76e01e14cc22e07fa379649
kexogg <kexogg@gmail.com>
2024-12-26 15:22:25 +0500

    remove unused code

--- TagsCloudContainerCore/Facade/TagCloudModule.cs
+++ TagsCloudContainerCore/Facade/TagCloudModule.cs
@@ -20,17 +20,12 @@ public class TagCloudModule : Module
     protected override void Load(ContainerBuilder builder)
     {
         builder.RegisterType<TagCloud>().AsSelf().InstancePerLifetimeScope();
-        /*builder.RegisterType<FileDataProvider>().As<IDataProvider>();
-        builder.RegisterType<MyStemWordProcessor>().As<IWordProcessor>();
-        builder.RegisterType<CircularClouldLayouterFactory>().As<ILayouterFactory>();
-        builder.RegisterType<Renderer.Renderer>().As<IRenderer>();
-        builder.RegisterType<PngEncoder>().As<IImageEncoder>();*/
         
-        builder.RegisterType(_options.DataProviderType).As<IDataProvider>();
-        builder.RegisterType(_options.WordProcessorType).As<IWordProcessor>();
-        builder.RegisterType(_options.LayouterType).As<ILayouterFactory>();
-        builder.RegisterType(_options.RendererType).As<IRenderer>();
-        builder.RegisterType(_options.ImageEncoderType).As<IImageEncoder>();
+        builder.RegisterType(_options.DataProviderType).As<IDataProvider>().SingleInstance();
+        builder.RegisterType(_options.WordProcessorType).As<IWordProcessor>().SingleInstance();
+        builder.RegisterType(_options.LayouterType).As<ILayouterFactory>().SingleInstance();
+        builder.RegisterType(_options.RendererType).As<IRenderer>().SingleInstance();
+        builder.RegisterType(_options.ImageEncoderType).As<IImageEncoder>().SingleInstance();
         
 
         foreach (var (serviceType, implementationType) in _options.ServiceMap)
